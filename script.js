2f11c56bf7e8e03b282564cde6ee50acbf7a7a91
Aman Chandra Dwivedi <143540782+acdengineer@users.noreply.github.com>
2024-10-20 10:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const h1=React.createElement("h1",{
-  id: "h1"
+  id: "heading",
+  className : "heading"
 },
-  React.createElement("h2",{},"hii"))
+  React.createElement("p",{null},"Hello React"))
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(h1)
